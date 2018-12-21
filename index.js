70fc751fba055f73d13c4ae594363d51f71b09c7
azizfcb <aziz50607080@gmail.com>
2018-12-21 12:55:52 +0100

    initial commit for fantasY Khniss demo

---
+++ index.js
@@ -0,0 +1,89 @@
+var azifpl = require('azifpl');
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/public'));
+
+function leaguePlayers(id, callback) {
+    azifpl.getClassicLeagueTopTenPlayers(id).then(function (res) {
+        callback(res)
+    }, function (err) {
+        callback(err)
+    })
+}
+function comparaison(id1, id2, callback) {
+    azifpl.TwoTeamsH2H(id1, id2).then(function (res) {
+        callback(res)
+    }, function (err) {
+        callback(err)
+    })
+}
+function cupLeagueStats(id, callback) {
+    azifpl.getCupTeams(id).then(function (res) {
+        callback(res)
+    }, function (err) {
+        callback(err)
+    })
+}
+function gameweekAverage(callback) {
+    azifpl.gameweekAverage().then(function (res) {
+        callback(res)
+    }, function (err) {
+        callback(err)
+    })
+}
+function highestLowest(id, callback) {
+    azifpl.highestLowest(id).then(function (res) {
+        callback(res)
+    }, function (err) {
+        callback(err)
+    })
+}
+function teamStats(id, callback) {
+    azifpl.teamStats(id).then(function (res) {
+        callback(res)
+    }, function (err) {
+        callback(err)
+    })
+}
+
+
+
+
+app.get('/league-data/:leagueId', function (req, res) {
+    leaguePlayers(req.params.leagueId, function (x) {
+        res.send(x);
+    });
+})
+app.get('/event-average', function (req, res) {
+    gameweekAverage(function (x) {
+        res.send(x);
+    });
+})
+app.get('/h2h/:id1/:id2', function (req, res) {
+    comparaison(req.params.id1, req.params.id2, function (x) {
+        res.send(x);
+    });
+})
+app.get('/lowest-highest/:leagueId', function (req, res) {
+    highestLowest(req.params.leagueId, function (x) {
+        res.send(x);
+    });
+})
+app.get('/cup/:leagueId', function (req, res) {
+    cupLeagueStats(req.params.leagueId, function (x) {
+        res.send(x);
+    });
+})
+app.get('/team-stats/:teamId', function (req, res) {
+     teamStats(req.params.teamId, function (x) {
+        res.send(x);
+    });
+})
+app.get('*', function (req, res) {
+    res.sendfile('./public/index.html');
+})
+app.listen(port, function () {
+    console.log('Example app listening on port 8080!');
+});
---
+++ public/app.js
@@ -0,0 +1,118 @@
+var app = angular.module("azzouz-fpl", []);
+app.run(function ($rootScope, $http) {
+    $http({
+        method: 'GET',
+        url: '/league-data/9908'
+    }).then(function successCallback(response) {
+        $rootScope.leaguePlayers = response.data
+        console.log($rootScope.leaguePlayers)
+    }, function errorCallback(response) {
+        console.log(response)
+    });
+})
+app.controller("mainCtrl", function ($scope, $http, $rootScope) {
+
+    $scope.leagueId = 9908;
+    $scope.maxi = function (x) {}
+    $rootScope.option = 0;
+
+    $scope.isOption = function (str) {
+        $scope.option == str;
+        $scope.h2hData = undefined
+        if (str == "cup") {
+            $scope.leaguePlayersInCup($scope.leagueId)
+        }
+        if (str == "average") {
+            $scope.gameweekAverage($scope.leagueId)
+        }
+        if (str == "highLow") {
+            $scope.highestLowest($scope.leagueId)
+        }
+        if (str == "stats") {
+//            $scope.TeamStats($scope.leagueId)
+        }
+    }
+    $scope.getTeamStats = function (teamId) {
+        $http({
+            method: 'GET',
+            url: '/team-stats/' + teamId
+        }).then(function successCallback(response) {
+            $scope.teamStats = response.data.data
+
+
+            console.log($scope.teamStats)
+        }, function errorCallback(response) {
+            console.log(response)
+        });
+    }
+    $scope.inCup = function (cupStatus) {
+        return cupStatus ? "" : "X"
+    }
+    $scope.gameweekAverage = function () {
+        $http({
+            method: 'GET',
+            url: '/event-average'
+        }).then(function successCallback(response) {
+            $scope.averageData = response.data
+        }, function errorCallback(response) {
+            console.log(response)
+        });
+    }
+    $scope.highestLowest = function (id) {
+        $http({
+            method: 'GET',
+            url: '/lowest-highest/' + id
+        }).then(function successCallback(response) {
+            $scope.highestGameweek = response.data.sort(compareGameweek)[0]
+            $scope.lowesttGameweek = response.data.sort(compareBench)[0]
+
+
+            console.log()
+        }, function errorCallback(response) {
+            console.log(response)
+        });
+    }
+
+    function compareGameweek(a, b) {
+        if (a.data.maxGameweekPoints.value < b.data.maxGameweekPoints.value)
+            return -1;
+        if (a.data.maxGameweekPoints.value > b.data.maxGameweekPoints.value)
+            return 1;
+        return 0;
+    }
+    function compareBench(a, b) {
+        if (a.data.maxBenchedPoints.value < b.data.maxBenchedPoints.value)
+            return -1;
+        if (a.data.maxBenchedPoints.value > b.data.maxBenchedPoints.value)
+            return 1;
+        return 0;
+    }
+    $scope.leaguePlayersInCup = function (leagueId) {
+        $http({
+            method: 'GET',
+            url: '/cup/' + leagueId
+        }).then(function successCallback(response) {
+            $scope.loading = false;
+            $scope.leaguePlayersInCup = response.data
+        }, function errorCallback(response) {
+            console.log(response)
+        });
+    }
+    $scope.h2h = function (x, y) {
+        if (x == undefined || y == undefined) {
+            alert('a5tar equipe yazzebi!')
+        } else if (x == y) {
+            alert('Ta5tar fi nafs joueur ya3tek 3asba')
+        } else {
+            $http({
+                method: 'GET',
+                url: '/h2h/' + x + '/' + y
+            }).then(function successCallback(response) {
+                $scope.h2hData = response.data
+                console.log(response)
+            }, function errorCallback(response) {
+                console.log(response)
+            });
+        }
+    }
+});
